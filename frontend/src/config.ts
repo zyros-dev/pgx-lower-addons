030f3088b870aad8f629302bb9fa656db847d5ae
zyros-dev <zyros.dev@gmail.com>
2025-10-05 13:28:31 +1100

    log the url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,7 @@
 const API_BASE_URL = process.env.REACT_APP_API_URL ||
   `${window.location.protocol}//${window.location.host}/api`;
 
+console.log('API_BASE_URL:', API_BASE_URL);
+console.log('window.location:', window.location.href);
+
 export { API_BASE_URL };
